f98df240e6bd1c9b4c49ab827555feacbe3d48f4
Blerton Ismaili <ismaili.blerton.03@gmail.com>
2026-06-02 19:34:59 +0200

    Added base endpoints for Products

--- src/main/java/com/seeu/resources/ProductResource.java
+++ src/main/java/com/seeu/resources/ProductResource.java
@@ -1,7 +1,10 @@
 package com.seeu.resources;
 
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 
 @Path("/products")
@@ -9,6 +12,23 @@ public class ProductResource extends BaseResource {
 
     @GET
     public Response getAll() {
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("/{product_id}")
+    public Response get(@PathParam("product_id") String productId) throws Exception {
+        return Response.ok().build();
+    }
+
+    @PUT
+    public Response save(String payload) throws Exception {
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Path("/{product_id}")
+    public Response delete(@PathParam("product_id") String productId) throws Exception {
         return Response.ok().build();
     }
 }
